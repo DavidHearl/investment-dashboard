47d0f399aac091e518cb336cfa6f4f593669d69f
DavidHearl <david.hearl@outlook.com>
2022-01-05 11:57:17 +0000

    added equations to js to add current totals to cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,88 @@
-var core_balance
+// Total Values
+var coreB = 0
+var generalB = 0
+var dividendB = 0
 
-let core_value = document.getElementsByClassName("core-value");
-let core_difference = document.getElementsByClassName("core-difference");
+var coreD = 0
+var generalD = 0
+var dividendD = 0
+
+// Card Parameters
+let totalBalance = document.getElementById("total-balance");
+let totalGain = document.getElementById("total-gain");
+
+let coreBalance = document.getElementById("core-balance");
+let coreGain = document.getElementById("core-gain");
+
+let generalBalance = document.getElementById("core-balance");
+let generalGain = document.getElementById("general-gain");
+
+let dividendBalance = document.getElementById("core-balance");
+let dividendGain = document.getElementById("dividend-gain");
+
+// Core
+let coreValue = document.getElementsByClassName("core-value");
+let coreDifference = document.getElementsByClassName("core-difference");
 
 for (i = 0; i < core.length; i++) {
-    core_value[i].innerHTML = "£" + core[i].value;
-    core_difference[i].innerHTML = "£" + core[i].gainLoss;
+    coreValue[i].innerHTML = "£" + core[i].value;
+    coreDifference[i].innerHTML = "£" + core[i].difference;
 
-    if (core[i].gainLoss < 0) {
-        core_difference[i].style.color = "red";
+    if (core[i].difference < 0) {
+        coreDifference[i].style.color = "red";
     } else {
-        core_difference[i].style.color = "green";
+        coreDifference[i].style.color = "green";
     }
-
-    parseInt(core_balance) = parseInt(core[i].value)
-    console.log(core_balance)
+    coreB += Number(core[i].value)
+    coreD += Number(core[i].difference)
 }
+
+coreBalance.innerHTML = "£" + coreB.toFixed(2)
+coreGain.innerHTML = "£" + coreD.toFixed(2)
+
+// General
+let generalValue = document.getElementsByClassName("general-value");
+let generalDifference = document.getElementsByClassName("general-difference");
+
+for (i = 0; i < general.length; i++) {
+    generalValue[i].innerHTML = "£" + general[i].value;
+    generalDifference[i].innerHTML = "£" + general[i].difference;
+
+    if (general[i].difference < 0) {
+        generalDifference[i].style.color = "red";
+    } else {
+        generalDifference[i].style.color = "green";
+    }
+    generalB += Number(general[i].value)
+    generalD += Number(general[i].difference)
+}
+
+generalBalance.innerHTML = "£" + generalB.toFixed(2)
+generalGain.innerHTML = "£" + generalD.toFixed(2)
+
+// Dividend
+let dividendValue = document.getElementsByClassName("dividend-value");
+let dividendDifference = document.getElementsByClassName("dividend-difference");
+
+for (i = 0; i < dividend.length; i++) {
+    dividendValue[i].innerHTML = "£" + dividend[i].value;
+    dividendDifference[i].innerHTML = "£" + dividend[i].difference;
+
+    if (dividend[i].difference < 0) {
+        dividendDifference[i].style.color = "red";
+    } else {
+        dividendDifference[i].style.color = "green";
+    }
+    dividendB += Number(dividend[i].value)
+    dividendD += Number(dividend[i].difference)
+}
+
+dividendBalance.innerHTML = "£" + dividendB.toFixed(2)
+dividendGain.innerHTML = "£" + dividendD.toFixed(2)
+
+
+
+
+
+totalBalance.innerHTML = "£" + (coreB + generalB + dividendB).toFixed(2)
+totalGain.innerHTML = "£" + (coreD + generalD + dividendD).toFixed(2)
